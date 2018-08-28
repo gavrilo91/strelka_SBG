085ac97602a23d6d36520f33cdc8ba81538e9425
gavrilo91 <gavriloandric@gmail.com>
2018-08-28 21:26:05 +0200

    Added more inputs and docs

--- tools/strelka.py
+++ tools/strelka.py
@@ -1,4 +1,5 @@
 from sbg import cwl
+from tools.doc import TOOL_DOC
 from subprocess import check_call, check_output
 
 
@@ -12,11 +13,13 @@ from subprocess import check_call, check_output
                                                   doc="All somatic Indels inferred in the tumor sample"),
                           somatic_indels_tbi=cwl.File(glob='results/variants/somatic.indels.vcf.gz.tbi')
                           ),
-             docker='images.sbgenomics.com/gavrilo_andric/strelka:1')
+             docker='images.sbgenomics.com/gavrilo_andric/strelka:1',
+             label="Strelka 2.9.7")
 def strelka(normal_bam: cwl.File(secondary_files='.bai',
-                                 doc='Normal sample BAM or CRAM file. (no default)'),
+                                 doc='Normal sample BAM or CRAM file.'),
             tumor_bam: cwl.File(secondary_files='.bai',
-                                doc='Tumor sample BAM or CRAM file. [required] (no default)'),
+                                doc='Tumor sample BAM or CRAM file.',
+                                required=True),
             reference_fasta: cwl.File(secondary_files='.fai',
                                       doc='samtools-indexed reference fasta file [required]'),
             indel_candidates: cwl.File(
@@ -25,22 +28,55 @@ def strelka(normal_bam: cwl.File(secondary_files='.bai',
                     'exist in the sample. The VCF must be tabix indexed. All indel alleles'
                     ' must be left-shifted/normalized, any unnormalized alleles will be '
                     'ignored. This option may be specified more than once, multiple input '
-                    'VCFs will be merged. (default: None)') = None,
+                    'VCFs will be merged.',
+                default='None') = None,
+            forced_gt: cwl.File(doc="Specify a VCF of candidate alleles. "
+                                    "These alleles are always evaluated and "
+                                    "reported even if they are unlikely to exist in the "
+                                    "sample. The VCF must be tabix indexed. All indel "
+                                    "alleles must be left-shifted/normalized, any unnormalized "
+                                    "allele will trigger a runtime error. This option may "
+                                    "be specified more than once, multiple input VCFs will "
+                                    "be merged. Note that for any SNVs provided in the VCF, "
+                                    "the SNV site will be reported (and for gVCF, excluded "
+                                    "from block compression), but the specific SNV "
+                                    "alleles are ignored.",
+                                default='None') = None,
+            exome: cwl.Bool(doc="Set options for exome or other targeted input: note in "
+                                "particular that this flag turns off high-depth filters") = False,
+            call_regions: cwl.File(doc="Optionally provide a bgzip-compressed/tabix-indexed BED "
+                                       "file containing the set of regions to call. No VCF "
+                                       "output will be provided outside of these regions. "
+                                       "The full genome will still be used to estimate statistics "
+                                       "from the input (such as expected depth per chromosome). "
+                                       "Only one BED file may be specified.",
+                                   default='Call the entire genome') = None,
+            scan_size_mb: cwl.Int(doc="Maximum sequence region size (in megabases) scanned by "
+                                      "each task during genome variant calling. (default: 12)",
+                                  default=12) = 12,
+            region: cwl.String(doc="Limit the analysis to one or more genome region(s) for "
+                                   "debugging purposes. If this argument is provided multiple"
+                                   " times the union of all specified regions will be analyzed. "
+                                   "All regions must be non-overlapping to get a meaningful "
+                                   "result. Examples: '--region chr20' (whole chromosome), "
+                                   "'--region chr2:100-2000 --region chr3:2500-3000' "
+                                   "(two regions)'. If this option is specified (one or more times) "
+                                   "together with the --callRegions BED file, then all "
+                                   "region arguments will be intersected with the "
+                                   "callRegions BED track.",
+                               default='None') = None
             ):
     """
-    Version: 2.9.7
-
-    This script configures Strelka somatic small variant calling.
-    You must specify an alignment file (BAM or CRAM) for each sample of a matched tumor-normal pair.
-
-    Configuration will produce a workflow run script which
-    can execute the workflow on a single node or through
-    sge and resume any interrupted execution.
 
     :param normal_bam:
     :param tumor_bam:
     :param reference_fasta:
     :param indel_candidates:
+    :param forced_gt:
+    :param exome:
+    :param call_regions:
+    :param scan_size_mb:
+    :param region:
     :return:
     """
     strelka_config_path = '/opt/bin/configureStrelkaSomaticWorkflow.py'
@@ -53,10 +89,26 @@ def strelka(normal_bam: cwl.File(secondary_files='.bai',
     if indel_candidates:
         strelka_cmd += ['--indelCandidates', indel_candidates['path']]
 
+    if forced_gt:
+        strelka_cmd += ['--forcedGT', forced_gt['path']]
+
+    if exome:
+        strelka_cmd += ['--exome']
+
+    if call_regions:
+        strelka_cmd += ['--callRegions', call_regions['path']]
+
+    strelka_cmd += ['--scanSizeMb', str(scan_size_mb)]
+    if region:
+        strelka_cmd += ['--region', region]
     check_output(strelka_cmd)
     check_call(['python', 'runWorkflow.py', '-m', 'local', '-j', '8'])
 
 
+strelka_app = strelka()
+strelka_app.doc = TOOL_DOC
+
 if __name__ == '__main__':
     TOOL = strelka()
+    TOOL.doc = TOOL_DOC
     TOOL.dump('test.cwl')
