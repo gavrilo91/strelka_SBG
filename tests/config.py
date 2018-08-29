cbcc8ac552923e00ec6ad8ca9c7d44cbce125b1c
gavrilo91 <gavriloandric@gmail.com>
2018-08-29 11:13:05 +0200

    updated config file

---
+++ tests/config.py
@@ -0,0 +1,11 @@
+"""
+ENV configs
+"""
+import os
+PROFILE = os.environ['PROFILE']
+"""
+profile listed inside
+                        ``$HOME/.sevenbridges/credentials``
+                        (eg. cgc, default, cavatica)
+"""
+TEST_PROJECT = os.environ['TEST_PROJECT']
